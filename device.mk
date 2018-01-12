f2dc427ed66c33fed80f40af40a545e3c1b6de7e
Bruno Martins <bgcngm@gmail.com>
2018-01-12 22:03:34 +0000

    land: Build Lineage vibrator HAL
    Change-Id: Id60fb36cbae9f4a6dfb0cd926e7c05b798c5699d

--- device.mk
+++ device.mk
@@ -365,8 +365,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-service.lineage
 
 # QCOM
 PRODUCT_COPY_FILES += \
