8b1153ba21f50c8f5ffb409dfe3f4aa7e0801590
akshatsinha2002 <sinhaakshat2012@gmail.com>
2018-03-31 18:20:40 +0000

    Bring back orginal vibrator hal
    This reverts commit f2dc427ed66c33fed80f40af40a545e3c1b6de7e.

--- device.mk
+++ device.mk
@@ -367,7 +367,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-service.lineage
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # QCOM
 PRODUCT_COPY_FILES += \
