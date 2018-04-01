4da1c24183e5dbf7155749770b6e164a9952c1e7
akshatsinha2002 <sinhaakshat2012@gmail.com>
2018-04-01 11:06:12 +0000

    Disbale CNM and enable Snap Cam

--- device.mk
+++ device.mk
@@ -413,3 +413,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(DEVICE_PATH)/wifi/WCNSS_qcom_cfg.ini:system/vendor/etc/wifi/WCNSS_qcom_cfg.ini
+
+
+DISABLE_CNM=true
